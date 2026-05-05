db12eed688ac12a555712b2de0dec7e4fcef6716
J0EYJ0ES <garuj10@outlook.com>
2026-05-05 15:26:14 -0700

    Created a script and a gameobject
    Created a script for the created gamemanager, spawns 4 gameobjects at random speeds at random times.

---
+++ P7JimenezUnit5/Assets/Scripts/GameManager.cs
@@ -0,0 +1,42 @@
+using TMPro;
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> targets;
+    private float spawnRate = 1.0f;
+    private int score;
+    public TextMeshProUGUI scoreText;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        StartCoroutine(SpawnTarget());
+        score = 0;
+        scoreText.text = "Score; " + score;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+        }
+    }
+
+}
+
+
+
