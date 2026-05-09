2bdfd654f9dc945ae5e7c45aee09f9be7a687910
J0EYJ0ES <garuj10@outlook.com>
2026-05-09 12:38:37 -0700

    completed all of non bonus steps and completed up to easy bonus step
    Created texts and created a new script for ui

--- P7JimenezUnit5/Assets/Scripts/GameManager.cs
+++ P7JimenezUnit5/Assets/Scripts/GameManager.cs
@@ -4,19 +4,28 @@ using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEditor.Build.Reporting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
     private float spawnRate = 1.0f;
     private int score;
+    public bool isGameActive;
     public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+    public Button restartButton;
+    public GameObject titleScreen;
+    public TextMeshProUGUI livesText;
+    private int lives;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        StartCoroutine(SpawnTarget());
-        score = 0;
+        
+
+     
         
     }
 
@@ -28,7 +37,7 @@ public class GameManager : MonoBehaviour
 
     IEnumerator SpawnTarget()
     {
-        while (true)
+        while (isGameActive)
         {
             yield return new WaitForSeconds(spawnRate);
             int index = Random.Range(0, targets.Count);
@@ -41,6 +50,44 @@ public class GameManager : MonoBehaviour
     {
         score += scoreToAdd;
         scoreText.text = "Score: " + score;
+    }
+
+    public void UpdateLives(int livesToChange)
+    {
+        lives += livesToChange;
+        livesText.text = "Lives: " + lives;
+        if (lives <= 0)
+        {
+            GameOver();
+        }
+    }
+
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
+        restartButton.gameObject.SetActive(true);
+        isGameActive = false;
+        
+
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        
+    }
+
+    public void StartGame(int difficulty)
+    {
+        isGameActive = true;
+        score = 0;
+        spawnRate /= difficulty;
+
+        StartCoroutine(SpawnTarget());
+        UpdateScore(0);
+        UpdateLives(3);
+
+        titleScreen.gameObject.SetActive(false);
     }
 
 }
--- P7JimenezUnit5/Assets/Scripts/Target.cs
+++ P7JimenezUnit5/Assets/Scripts/Target.cs
@@ -16,6 +16,8 @@ public class Target : MonoBehaviour
 
     public int pointValue;
 
+    public ParticleSystem explosionParticle;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -31,13 +33,23 @@ public class Target : MonoBehaviour
     public void OnMouseDown()
     {
         Destroy(gameObject);
+        Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
         gameManager.UpdateScore(pointValue);
        
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        Destroy(gameObject);
+        if (gameManager.isGameActive)
+        {
+            Destroy(gameObject);
+            if (!gameObject.CompareTag("Bad") && gameManager.isGameActive)
+            {
+                gameManager.UpdateLives(-1);
+            }
+        }
+
+       
     }
 
     Vector3 RandomForce()
